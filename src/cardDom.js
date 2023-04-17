c34c72e9ff9b3a87da7aa980fed22f5f79f45647
unbearabletruth <antaarty@gmail.com>
2023-04-17 17:38:51 +0700

    cardDom fillNewCard now multiple functions

--- src/cardDom.js
+++ src/cardDom.js
@@ -3,59 +3,82 @@ import { ToDoCard } from "./classes";
 const addCard = document.querySelector("button.addCard");
 const content = document.querySelector(".todos");
 
+
+function formTitle(){
+    const titleinput = document.createElement("input");
+    titleinput.type = "text";
+    titleinput.placeholder = "Title"
+    return titleinput;
+}
+
+function formDescription(){
+    const descriptioninput = document.createElement("input");
+    descriptioninput.type = "text";
+    descriptioninput.placeholder = "Description"
+    return descriptioninput;
+}
+
+function formDueDate(){
+    const dueDateinput = document.createElement("input");
+    dueDateinput.type = "date";
+    dueDateinput.placeholder = "Due date";
+    return dueDateinput;
+}
+
+function formPriority(){
+    const priorityinput = document.createElement("input");
+    priorityinput.type = "number";
+    priorityinput.placeholder = "priority";
+    return priorityinput;
+}
+
+function formSubmit(form, project, titleinput, descriptioninput, dueDateinput, priorityinput){
+    const submit = document.createElement("button");
+    submit.innerText = "Add";
+    submit.addEventListener("click", (e) => {
+        form.remove(); 
+        const newToDo = new ToDoCard(titleinput.value, descriptioninput.value, dueDateinput.value, priorityinput.value);
+        project.add(newToDo);
+        project.showAllCards();
+        renderNewCard(newToDo);
+        e.preventDefault();   
+    });
+    return submit;
+}
+
+function formCancel(form){
+    const cancel = document.createElement("button");
+    cancel.innerText = "Cancel";
+    cancel.addEventListener("click", (e) => {
+        e.preventDefault();  
+        form.remove();
+    })
+    return cancel;
+}
+
 function fillNewCard(project){
     addCard.addEventListener("click", () => {
         const form = document.createElement("form");
         form.id = "cardForm";
-        const titlelabel = document.createElement("label");
-        titlelabel.textContent = "Title";
-        const titleinput = document.createElement("input");
-        titleinput.type = "text";
-            
-        const descriptionlabel = document.createElement("label");
-        descriptionlabel.textContent = "Description";
-        const descriptioninput = document.createElement("input");
-        descriptioninput.type = "text";
-            
-        const dueDatelabel = document.createElement("label");
-        dueDatelabel.textContent = "Due date";
-        const dueDateinput = document.createElement("input");
-        dueDateinput.type = "date";
-               
-        const prioritylabel = document.createElement("label");
-        prioritylabel.textContent = "Priority";
-        const priorityinput = document.createElement("input");
-        priorityinput.type = "number";
-            
-        const submit = document.createElement("button");
-        submit.innerText = "Add";
-            
-        form.appendChild(titlelabel);
+        const titleinput = formTitle();
+        const descriptioninput = formDescription();
+        const dueDateinput = formDueDate();
+        const priorityinput = formPriority(); 
+        const submit = formSubmit(form, project, titleinput, descriptioninput, dueDateinput, priorityinput);
+        const cancel = formCancel(form);
         form.appendChild(titleinput);
-        form.appendChild(descriptionlabel);
         form.appendChild(descriptioninput);    
-        form.appendChild(dueDatelabel);
         form.appendChild(dueDateinput);
-        form.appendChild(prioritylabel);
         form.appendChild(priorityinput);
         form.appendChild(submit);
-        content.appendChild(form);
-        submit.addEventListener("click", (e) => {
-            form.remove(); 
-            const newToDo = new ToDoCard(titleinput.value, descriptioninput.value, dueDateinput.value, priorityinput.value);
-            project.add(newToDo);
-            project.showAllCards();
-            renderNewCard(newToDo);
-                 
-            e.preventDefault();   
-        });
+        form.appendChild(cancel);
+        content.appendChild(form); 
     })
 }
 
 function renderNewCard(card){
     const toDo = document.createElement("div");
     toDo.classList.add("toDocard");
-        
     const title1 = document.createElement("div");
     title1.textContent = `${card.title}`;
     const description1 = document.createElement("div");
