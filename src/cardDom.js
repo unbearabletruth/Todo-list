098c5822ac7c417cc3129a259b9d9fe688f8aa66
unbearabletruth <antaarty@gmail.com>
2023-04-21 11:10:40 +0700

    minor css changes

--- src/cardDom.js
+++ src/cardDom.js
@@ -71,7 +71,7 @@ function renderDueDate(card){
     title.textContent = "Due date";
     const text = document.createElement("p");
     text.classList.add("renderedCardText");
-    const formatDate = format(parseISO(card.dueDate), 'dd MMM yy HH:mm')
+    const formatDate = format(parseISO(card.dueDate), 'dd MMM yy HH:mm')//how to change date placeholder
     text.textContent = `${formatDate}`;
     //text.textContent = `${card.dueDate}`;
     dueDate.appendChild(title);
